544bfe22853aa02969047a9e6ad18983b768a1d9
Sathavan18 <bt21583@qmul.ac.uk>
2026-05-05 15:14:34 +0100

    Added comments to mental directory

--- mental/urls.py
+++ mental/urls.py
@@ -1,8 +1,15 @@
+"""
+URL routing for mental health tracking endpoints.
+Maps URLs to views for journal entries, mood ratings, and meditation sessions.
+"""
 from django.urls import path
 from .views import JournalEntryListCreateView, MoodRatingListCreateView, MeditationSessionListCreateView
 
 urlpatterns = [
+    # Journal entries endpoint - create and list journal entries
     path('journal/', JournalEntryListCreateView.as_view(), name='journal-entries'),
+    # Mood ratings endpoint - create and list mood ratings
     path('mood/', MoodRatingListCreateView.as_view(), name='mood-ratings'),
+    # Meditation sessions endpoint - create and list meditation sessions
     path('meditation/', MeditationSessionListCreateView.as_view(), name='meditation-sessions'),
 ]
