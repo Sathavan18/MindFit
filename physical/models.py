ac296997876107a7d4bf6a230bf10aeb8e336dfa
Sathavan18 <bt21583@qmul.ac.uk>
2026-05-05 15:21:52 +0100

    Added comments to views.py

--- physical/models.py
+++ physical/models.py
@@ -1,8 +1,17 @@
+"""
+Models for physical health tracking features.
+Stores weight entries with calorie intake linked to users.
+"""
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
 class WeightEntry (models.Model):
+    """
+    WeightEntry tracks daily weight and calorie intake.
+    Used for weight progress visualisation and analysis.
+    Linked to mood data in Insights page to show physical-mental health correlations.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     weight = models.FloatField()
     date = models.DateField(default=timezone.now)
--- physical/urls.py
+++ physical/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import WeightEntryListCreateView
 
 urlpatterns = [
+    # Weight entries endpoint - create and list weight entries
     path('weight/', WeightEntryListCreateView.as_view(), name='weight-entries'),
 ]
